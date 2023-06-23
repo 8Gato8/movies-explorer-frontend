6083ed7ecb537103ea0d3baa1ae000c840bacad7
Sergey Skotcelyas <sergey.sckotcelias@yandex.ru>
2023-06-23 23:42:41 +0800

    feat: Cверстать адаптивный about-project

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function AboutProject() {
+	return (
+		<section className="about-project">
+			<h1 className="about-project__section-heading section-heading">О проекте</h1>
+
+			<div className="about-project__info-block">
+				<h2 className="about-project__sub-heading">Дипломный проект включал 5&nbsp;этапов</h2>
+				<p className="about-project__info">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и&nbsp;финальные доработки.</p>
+			</div>
+
+			<div className="about-project__info-block">
+				<h2 className="about-project__sub-heading">На&nbsp;выполнение диплома ушло 5&nbsp;недель</h2>
+				<p className="about-project__info">У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+			</div>
+
+			<div className="about-project__time-investment-bars">
+				<div className="about-project__bar about-project__bar_type_backend">1 неделя</div>
+				<p className="about-project__caption">Back-end</p>
+				<div className="about-project__bar about-project__bar_type_frontend">4 недели</div>
+				<p className="about-project__caption">Front-end</p>
+			</div>
+		</section>
+	)
+}
+
+export default AboutProject;
