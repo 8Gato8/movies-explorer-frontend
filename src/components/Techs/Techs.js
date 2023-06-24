e1ab576379fed0b37f8021402ecedf0bccd2c967
Sergey Skotcelyas <sergey.sckotcelias@yandex.ru>
2023-06-24 22:27:59 +0800

    feat: Сверстать адаптивный techs

---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Techs() {
+	return (
+		<div className="techs-wrapper">
+			<section className="techs">
+				<h1 className="techs__section-heading section-heading">Технологии</h1>
+
+				<h2 className="techs__sub-heading">7 технологий</h2>
+
+				<p className="techs__info">На&nbsp;курсе веб-разработки мы&nbsp;освоили технологии, которые применили в&nbsp;дипломном проекте.</p>
+
+				<ul className="techs__tech-list">
+					<li className="techs__tech">HTML</li>
+					<li className="techs__tech">CSS</li>
+					<li className="techs__tech">JS</li>
+					<li className="techs__tech">React</li>
+					<li className="techs__tech">Git</li>
+					<li className="techs__tech">Express.js</li>
+					<li className="techs__tech">mongoDB</li>
+				</ul>
+			</section>
+		</div>
+	)
+}
+
+export default Techs;
