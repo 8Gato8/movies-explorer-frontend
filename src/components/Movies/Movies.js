d6328c9b31d447e48c8ffbb10bfbf3f22e5507dd
Sergey Skotcelyas <sergey.sckotcelias@yandex.ru>
2023-07-10 18:45:10 +0800

    fix: Задать минимальную высоту для блока movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,6 +2,8 @@ import React from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
+import './Movies.css';
+
 function Movies({ setCurrentPath, currentPath, movies }) {
 
 	React.useEffect(() => {
