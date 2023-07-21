61b2dabfeaaa670c8c1cb380b04b3227f3f17eb8
Sergey Skotcelyas <sergey.sckotcelias@yandex.ru>
2023-07-21 16:01:17 +0800

    fix: Исправить путь к api

--- src/utils/constants/baseUrls.js
+++ src/utils/constants/baseUrls.js
@@ -1,3 +1,2 @@
-/* export const BASE_URL_OWN_API = 'https://gato.diploma.nomoredomains.rocks'; */
-export const BASE_URL_OWN_API = 'http://localhost:3001';
+export const BASE_URL_OWN_API = 'https://gato.diploma.nomoredomains.rocks';
 export const BASE_URL_BEAT_FILM_API = 'https://api.nomoreparties.co/beatfilm-movies';
