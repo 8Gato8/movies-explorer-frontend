4a32ef4c104752858ec95cdff65cee76c3f4d99e
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2023-10-12 17:25:24 +0800

    feat: Добавить настоящую фотографию

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,24 +5,40 @@ import './AboutMe.css';
 import authorPhotoSrc from '../../images/photo.jpg';
 
 function AboutMe() {
-	return (
-		<section className="about-me">
-			<h2 className="about-me__section-heading section-heading">Студент</h2>
+  return (
+    <section className="about-me">
+      <h2 className="about-me__section-heading section-heading">Студент</h2>
 
-			<div className="about-me__info-block">
+      <div className="about-me__info-block">
+        <h3 className="about-me__name">Сергей</h3>
+        <h3 className="about-me__occupation">Фронтенд-разработчик, 26 лет</h3>
 
-				<h3 className="about-me__name">Виталий</h3>
-				<h3 className="about-me__occupation">Фронтенд-разработчик, 30 лет</h3>
+        <p className="about-me__biography">
+          Я&nbsp;родился и&nbsp;живу в&nbsp;Иркутске, закончил юридический
+          факультет РГУП. Я&nbsp; всей душой люблю спорт, музыку, а&nbsp;ещё
+          люблю читать научпоп. Кодить начал в 2021 году по видеурокам на
+          YouTube. С 2018 по 2021 работал в&nbsp;сфере продаж. После того, как
+          решил пройти курс по&nbsp;веб-разработке, уволился с работы, чтобы
+          полностью посвятить себя обучению.
+        </p>
 
-				<p className="about-me__biography">Я&nbsp;родился и&nbsp;живу в&nbsp;Саратове, закончил факультет экономики СГУ. У&nbsp;меня есть жена и&nbsp;дочь. Я&nbsp;люблю слушать музыку, а&nbsp;ещё увлекаюсь бегом. Недавно начал кодить.
-					С 2015 года работал в&nbsp;компании &laquo;СКБ Контур&raquo;. После того, как прошёл курс по&nbsp;веб-разработке, начал заниматься фриланс-заказами и ушёл с&nbsp;постоянной работы.</p>
+        <a
+          href="https://github.com/8Gato8"
+          className="about-me__link"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Github
+        </a>
+      </div>
 
-				<a href="https://github.com/8Gato8" className="about-me__link" rel="noreferrer" target="_blank">Github</a>
-			</div>
-
-			<img src={authorPhotoSrc} alt="фотография автора проекта" className="about-me__photo" />
-		</section>
-	)
+      <img
+        src={authorPhotoSrc}
+        alt="фотография автора проекта"
+        className="about-me__photo"
+      />
+    </section>
+  );
 }
 
 export default AboutMe;
